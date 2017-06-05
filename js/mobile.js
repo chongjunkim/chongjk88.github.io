485873c95ebee05481ff75c9dc04ff6dac5e1359
Jun <chongjk88@hotmail.com>
2017-06-04 18:03:51 -0700

    mobile update

--- js/mobile.js
+++ js/mobile.js
@@ -31,6 +31,15 @@ function project() {
 		$(".m_steps0").removeClass("on");
 		$(".m_steps3").addClass("on");
 	});
+	$(".m_site3").on("swipeleft", function () {
+		$(".m_site3").fadeOut();
+		setTimeout(function () {
+			$(".m_site4").fadeIn(1000);
+		}, 100);
+		
+		$(".m_steps0").removeClass("on");
+		$(".m_steps4").addClass("on");
+	});
 	$(".m_site2").on("swiperight", function () {
 		$(".m_site2").fadeOut();
 		setTimeout(function () {
@@ -49,10 +58,22 @@ function project() {
 		$(".m_steps0").removeClass("on");
 		$(".m_steps2").addClass("on");
 	});
+	$(".m_site4").on("swiperight", function () {
+		$(".m_site4").fadeOut();
+		setTimeout(function () {
+			$(".m_site3").fadeIn(1000);
+		}, 100);
+		
+		$(".m_steps0").removeClass("on");
+		$(".m_steps3").addClass("on");
+	});
 	
+    
+    
 	$(".m_steps1").click(function () {
 		$(".m_site2").fadeOut();
 		$(".m_site3").fadeOut();
+		$(".m_site4").fadeOut();
 		setTimeout(function () {
 			$(".m_site1").fadeIn(1000);
 		}, 100);
@@ -63,6 +84,7 @@ function project() {
 	$(".m_steps2").click(function () {
 		$(".m_site1").fadeOut();
 		$(".m_site3").fadeOut();
+		$(".m_site4").fadeOut();
 		setTimeout(function () {
 			$(".m_site2").fadeIn(1000);
 		}, 100);
@@ -71,13 +93,25 @@ function project() {
 		$(".m_steps2").addClass("on");
 	});
 	$(".m_steps3").click(function () {
-		$(".m_site2").fadeOut();
 		$(".m_site1").fadeOut();
+		$(".m_site2").fadeOut();
+		$(".m_site4").fadeOut();
 		setTimeout(function () {
 			$(".m_site3").fadeIn(1000);
 		}, 100);
 		
 		$(".m_steps0").removeClass("on");
 		$(".m_steps3").addClass("on");
+	});
+	$(".m_steps4").click(function () {
+		$(".m_site1").fadeOut();
+		$(".m_site2").fadeOut();
+		$(".m_site3").fadeOut();
+		setTimeout(function () {
+			$(".m_site4").fadeIn(1000);
+		}, 100);
+		
+		$(".m_steps0").removeClass("on");
+		$(".m_steps4").addClass("on");
 	});
 }
